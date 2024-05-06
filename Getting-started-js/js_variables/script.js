8744381bfdc0c631e7fba5f790bd51f234b67476
utsav-baghela <utsavu858@gmail.com>
2024-05-06 15:41:18 +0530

    types of variable in js

---
+++ Getting-started-js/js_variables/script.js
@@ -0,0 +1,14 @@
+// using var
+// storing a person's name
+
+// var firstName = "Utsav";
+// console.log(firstName);
+
+//using let
+
+// let firstName = "Vishal";
+// console.log(firstName);
+
+// using const
+const firstName = "Vishal";
+console.log(firstName);
