646c44a3ed0cd9f034030bf1bcda184cac6e1acd
utsav-baghela <utsavu858@gmail.com>
2024-05-07 16:44:22 +0530

    comparison operators

---
+++ Getting-started-js/operators/comparison operator/script.js
@@ -0,0 +1,23 @@
+let ageOfUb = 25;
+let ageOfVb = 30;
+let userInput = "25";
+// using == { it compares value not data}
+
+// console.log(ageOfUb == userInput);
+
+// using === { strictly equal not just value}
+// console.log(ageOfUb === userInput);
+
+//using != 
+// console.log(ageOfUb != userInput);
+
+//using !==
+// console.log(ageOfUb !== userInput);
+
+
+//using >
+console.log(ageOfUb > ageOfVb);
+
+// using <= >=
+// console.log(ageOfUb >= ageOfVb);
+// console.log(ageOfUb <= ageOfVb);
