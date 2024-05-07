4b2592f639fafcae614a3fa0732596b85d89e675
utsav-baghela <utsavu858@gmail.com>
2024-05-07 11:46:46 +0530

    arithemetic operators

---
+++ Getting-started-js/operators/script.js
@@ -0,0 +1,18 @@
+//modulus 
+const results = 10 % 2;
+//console.log(results);
+
+// let number = 10;
+// number ++ ;
+// console.log(number);
+
+// let score = 5;
+
+// score += 1;
+// console.log(score);
+
+// let a = 4 + 6;
+// console.log(a);
+let score = 5;
+score -= 2;
+console.log(score);
