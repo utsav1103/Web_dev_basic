4869e8fe8b05ec209f4909f1f923cf3787eaa49c
utsav-baghela <utsavu858@gmail.com>
2024-05-07 11:23:04 +0530

    data types in js

---
+++ Getting-started-js/data-type/script.js
@@ -0,0 +1,30 @@
+// number 
+const age = 25;
+
+// String
+const name = "UTASV"
+
+// Boolean
+const isLoggedIn = false;
+
+//null
+const salary = null;
+
+// undefined
+
+let numOfChildren ;
+//console.log(numOfChildren);
+
+//!-- non primitive data type ---
+
+//object
+
+const user = {
+    firstName : "UB",
+    lastName : "VB",
+    age : 28,
+};
+// console.log(user);
+
+const number = [10,"jan",true, null, undefined,{}];
+console.log(number);
