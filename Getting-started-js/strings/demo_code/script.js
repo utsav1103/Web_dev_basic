892a62a1b3994c78ee591cac9903f1e6abc1ebbf
utsav-baghela <utsavu858@gmail.com>
2024-05-08 18:19:45 +0530

    string demo code

---
+++ Getting-started-js/strings/demo_code/script.js
@@ -0,0 +1,16 @@
+//==
+
+// single, double and constructor===
+
+
+
+//single quotes
+const singleQuoteString = 'This is a string using single quotes';
+// console.log(singleQuoteString);
+
+const DoubleQuoteString = 'This is a string using double quotes';
+// console.log(DoubleQuoteString);
+
+//using the string constructor
+const stringConstructor = new String("This is a string using the string constructor quotes");
+console.log(stringConstructor);
