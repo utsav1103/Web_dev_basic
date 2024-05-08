b6c5aefa6c85d77db3e1c5380a0f3dc30eec45d5
utsav-baghela <utsavu858@gmail.com>
2024-05-08 18:34:38 +0530

    template literals backticks

---
+++ Getting-started-js/strings/template_literals/script.js
@@ -0,0 +1,32 @@
+// template literals
+
+const greeting = `Hello Witch`;
+console.log(greeting);
+
+
+/// string interploation
+
+const username = 'Darlin...'
+const welcomeMessage = `HEllO, ${username}! welcome to my page`;
+ 
+console.log(welcomeMessage)
+
+
+//multiline string
+
+const poem =`roses are red,
+violet are blue,
+Sugar is sweet ,
+And so are you?`
+
+console.log(poem);
+
+// expression evaluation
+
+const price = 10;
+const tax = 0.05;
+const total =`The price: ${price * (1 + tax)}`;
+
+// escaping backticks
+
+const stringWithBacktick = `he's a backtick \``;
