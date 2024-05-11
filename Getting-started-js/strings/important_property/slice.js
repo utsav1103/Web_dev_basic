0a9711484b75d085ceafad6a842fa7842998b850
utsav-baghela <utsavu858@gmail.com>
2024-05-11 17:54:10 +0530

    slice method

---
+++ Getting-started-js/strings/important_property/slice.js
@@ -0,0 +1,12 @@
+// Basic use of slice
+
+const greeting = 'Hello , world';
+const extractWord = greeting.slice(7, 13);
+console.log(extractWord);
+
+const text = 'Hello Senorita';
+const result = text.slice(7);
+
+const text1 = 'Hello darlin';
+const endText = text1.slice(-6);
+console.log(endText);
