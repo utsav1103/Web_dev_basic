219a0e2e8fe28331fb2dde05b05aa8fbf4f46016
utsav-baghela <utsavu858@gmail.com>
2024-05-13 15:49:12 +0530

    understanding idexof method

--- Getting-started-js/strings/important_property/slice.js
+++ Getting-started-js/strings/important_property/slice.js
@@ -1,37 +1,51 @@
 // Basic use of slice
 
-const greeting = 'Hello , world';
+const greeting = "Hello , world";
 const extractWord = greeting.slice(7, 13);
 console.log(extractWord);
 
-const text = 'Hello Senorita';
+const text = "Hello Senorita";
 const result = text.slice(7);
 
-const text1 = 'Hello darlin';
+const text1 = "Hello darlin";
 const endText = text1.slice(-6);
 console.log(endText);
 
-
-// using string split method
+//------ using string split method
 
 //simple split
 
-const sentence = 'Hello, Nejuko';
-const words = sentence.split(' ');
+const sentence = "Hello, Nejuko";
+const words = sentence.split(" ");
 console.log(words);
 
 //limit number split
 
 const myText = "apple , banana, pear";
-const mimit = myText.split(',',2)
+const mimit = myText.split(",", 2);
 
 // split by multiple characters
 
-const complex = "apple:banna;you"
+const complex = "apple:banna;you";
 const fruits = complex.split(/[:;]/);
 
 // split into characters
 
-const wor = 'MyDarlin';
+const wor = "MyDarlin";
 const vor = wor.split("");
 
+//------.indexof() method
+
+//basic usage
+
+const senten = "Konnichiwa";
+const index1 = senten.indexOf("w");
+
+//----using string .trim property
+
+// const sentences = '  arigato@gmail.com   '
+// const email = sentences.trim()
+
+// // removing line break
+// const input = '\n\t Hello, world';
+// const output = input.trim()
