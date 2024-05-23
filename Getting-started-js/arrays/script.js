aee53821965c98a1ef5bd7c720e17aa82d556abe
utsav-baghela <utsavu858@gmail.com>
2024-05-23 13:42:08 +0530

    declaring array using array consturctor

--- Getting-started-js/arrays/script.js
+++ Getting-started-js/arrays/script.js
@@ -6,19 +6,51 @@
 
 //syntax
 
-const studentAges = [20, 30, 14, 21]
-console.log(studentAges);
+// const studentAges = [20, 30, 14, 21]
+// console.log(studentAges);
 
 // declaring an array of String
 
-const daysOfWeek = ['mon', 'tue', 'wed','thru', 'fri','sat','sun']
-console.log(daysOfWeek);
+// const daysOfWeek = ['mon', 'tue', 'wed','thru', 'fri','sat','sun']
+// console.log(daysOfWeek);
 
 
-// declaring an array of mix types 
+//  declaring an array of mix types 
 
-const mixedType = [20, 'life', true]
+// const mixedType = [20, 'life', true]
 
-console.log(mixedType);
+// console.log(mixedType);
 
-const emptyArray = [];
+// const emptyArray = [];
+
+
+
+
+
+
+
+//-----
+// Array using array constructor
+
+//-----
+
+const emptyArr = new Array();
+
+// creating an array with predefined size (5)
+
+const sizeDefineArr = new Array(5)
+
+
+console.log(sizeDefineArr);
+
+// creating an array with elements
+
+const fruitArr = new Array('Cherry','Strawberry','Grapes')
+console.log(fruitArr);
+
+
+// creating an array of mixed type
+
+const mixedType1 = new Array(1, false, 'Darlin')
+
+console.log(mixedType1);
