f921b348a9a53182bd4ca1e7cd73118d881cfef7
utsav-baghela <utsavu858@gmail.com>
2024-05-23 13:29:43 +0530

    declaring array using array literal

---
+++ Getting-started-js/arrays/script.js
@@ -0,0 +1,24 @@
+//====
+
+// creating array using array literal
+
+// =====
+
+//syntax
+
+const studentAges = [20, 30, 14, 21]
+console.log(studentAges);
+
+// declaring an array of String
+
+const daysOfWeek = ['mon', 'tue', 'wed','thru', 'fri','sat','sun']
+console.log(daysOfWeek);
+
+
+// declaring an array of mix types 
+
+const mixedType = [20, 'life', true]
+
+console.log(mixedType);
+
+const emptyArray = [];
