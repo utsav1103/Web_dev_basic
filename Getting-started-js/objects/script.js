50384b29885cebf070b82288dbdb6c93b85d5cec
utsav-baghela <utsavu858@gmail.com>
2024-05-24 18:51:01 +0530

    understanding objects & using object literals

---
+++ Getting-started-js/objects/script.js
@@ -0,0 +1,10 @@
+//-=-=-><?<]]
+//creating object using object literals
+
+const book = {
+    title:'Harry Potter',
+    author:'J.K Rowlling',
+    available:true,
+    price:19.99
+}
+console.log(book);
