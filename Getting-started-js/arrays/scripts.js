18bec183271ef00b150b29afc6b8b9cf2eece9b2
utsav-baghela <utsavu858@gmail.com>
2024-05-24 12:37:33 +0530

    using reducing .reduce () method

--- Getting-started-js/arrays/scripts.js
+++ Getting-started-js/arrays/scripts.js
@@ -48,4 +48,42 @@ const mix = [0,1,'app',"",null,'kiwi']
 const truth = mix.filter(function(value){
     return Boolean(value)
 })
-console.log(truth);
+// console.log(truth);
+
+
+
+
+
+
+
+
+///-==-=-=-
+
+//using reduce or .reduce method
+
+//=--=-=-=-
+
+
+const number1 = [1,2,3,4,5,6,7,8,9,0]
+
+const sum1 = number1.reduce(function(acc, cuurent){
+    //reduce logic
+    return acc + cuurent
+}, 0)// zero here is the accumulator
+//  console.log(sum1)
+
+
+//maximun number
+const maxnum = number1.reduce(function(acc, cuurent){
+    if(cuurent>acc){
+        return cuurent
+    }else{
+        return acc
+    }
+},0)
+// console.log(maxnum);
+
+
+
+
+
