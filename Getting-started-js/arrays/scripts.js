674baacbce89e28138513072207d85617d2bd773
utsav-baghela <utsavu858@gmail.com>
2024-05-24 12:17:49 +0530

    using .filter in array

--- Getting-started-js/arrays/scripts.js
+++ Getting-started-js/arrays/scripts.js
@@ -2,11 +2,11 @@
 //-=-=-=
 
 
-const numbers = [1,2,3,4,5,6]
+// const numbers = [1,2,3,4,5,6]
 
-const newNumArrSqur = numbers.map(function(num , idx, arr){
-    return num * 10
-})
+// const newNumArrSqur = numbers.map(function(num , idx, arr){
+//     return num * 10
+// })
 
 // console.log(newNumArrSqur);
 /// 
@@ -17,3 +17,35 @@ const conWord = words.map(function(word){
     return word.toUpperCase()
 })
 // console.log(conWord)
+
+
+
+
+
+
+
+
+
+
+
+//=-=-=-=
+
+// filter method in array
+
+//=-=-=-=-
+
+const numbers = [1,2,3,4,5,6,7,8,9,0]
+
+const even = numbers.filter(function(num){
+    //filter logic
+    return num % 2 === 0
+})
+// console.log(even);
+
+
+const mix = [0,1,'app',"",null,'kiwi']
+
+const truth = mix.filter(function(value){
+    return Boolean(value)
+})
+console.log(truth);
