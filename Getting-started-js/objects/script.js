6d08b1555921d70ee8637453c417796d92d8b1ce
utsav-baghela <utsavu858@gmail.com>
2024-05-24 19:10:40 +0530

    using [] bracket notations

--- Getting-started-js/objects/script.js
+++ Getting-started-js/objects/script.js
@@ -19,7 +19,36 @@ const book = {
 // dot notation (.name)
 
 const bookTitle = book.title
-console.log(bookTitle)
-console.log(book.author)
-console.log(book.price)
-console.log(book.available)
+// console.log(bookTitle)
+// console.log(book.author)
+// console.log(book.price)
+// console.log(book.available)
+
+
+ 
+
+
+//-=--=__-=+_-
+
+// brackets notations
+
+const books = {
+    title:'Harry Potter',
+    author:'J.K Rowlling',
+    available:true,
+    price:19.99
+}
+
+// const bookTitle1 = book['title']
+// console.log(book['available']);
+// console.log(book['price']);
+// console.log(book['author']);
+// console.log(book['title']);
+
+const person = {
+    'first name': 'UB',
+    'last name': 'TK',
+}
+
+console.log(person['first name']);
+console.log(person['last name']);
