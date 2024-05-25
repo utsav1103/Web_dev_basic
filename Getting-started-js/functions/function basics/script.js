98830acdfcff4321834a54a024452b7b85fb546e
utsav-baghela <utsavu858@gmail.com>
2024-05-25 13:19:41 +0530

    function Expressions

--- Getting-started-js/functions/function basics/script.js
+++ Getting-started-js/functions/function basics/script.js
@@ -1,24 +1,51 @@
 //==
 // syntax of function declaration
 
-function sayHello (){
-    //logic goes here
-    //write any logic
-    console.log('HELLO DARLIN');
-}
+// function sayHello (){
+//     //logic goes here
+//     //write any logic
+//     console.log('HELLO DARLIN');
+// }
 
 //calling the function or invoke or use the function
- sayHello()
+//  sayHello()
 
  //another function
  function greetings (){
     console.log('Welcome to My WevPage');
  }
-greetings()
+// greetings()
 
  function showDate(){
     const currDate = new Date()
     console.log(`Today's date is ${currDate}`);
  }
- showDate()
+//  showDate()
+
+
+
+///----
+/// Function expression
+///----
+
+const sayHello = function(){
+    console.log('HELLO DARLIN')
+}
+
+// sayHello()
+
+
+//example even or odd
+
+const evenOdd = function(){
+    const number = 13;
+    if(number % 2 === 0){
+        console.log('This is Even');
+    }else{
+        console.log('This is Odd');
+    }
+}
+// evenOdd()
+
+
 
