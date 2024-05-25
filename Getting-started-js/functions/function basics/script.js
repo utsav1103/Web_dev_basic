fc130cf561bc3bd45c11dac4dc6f17cdd65eb6b0
utsav-baghela <utsavu858@gmail.com>
2024-05-25 13:05:36 +0530

    how to call functions

--- Getting-started-js/functions/function basics/script.js
+++ Getting-started-js/functions/function basics/script.js
@@ -6,3 +6,19 @@ function sayHello (){
     //write any logic
     console.log('HELLO DARLIN');
 }
+
+//calling the function or invoke or use the function
+ sayHello()
+
+ //another function
+ function greetings (){
+    console.log('Welcome to My WevPage');
+ }
+greetings()
+
+ function showDate(){
+    const currDate = new Date()
+    console.log(`Today's date is ${currDate}`);
+ }
+ showDate()
+
