401c54f069bddba76e6cb25b6ba6057178847300
utsav-baghela <utsavu858@gmail.com>
2024-05-31 11:25:24 +0530

    destructuring in JS es6

--- Getting-started-js/functions/es6/script.js
+++ Getting-started-js/functions/es6/script.js
@@ -20,3 +20,36 @@ const greet1 = greet('TISA')
 
 const sqr = x => x * x
 const sqr1 = sqr(4)
+
+
+
+
+//>>>>>
+//destructuring in JS ES6
+//<><><>
+
+const numbers = [1,2,3,4];
+const [first , second, third, fourth] = numbers
+// console.log(first); // this is destructuring
+
+let a = 1;
+let b = 2;
+[a,b] = [b,a];
+
+// console.log(a,b);
+
+const {names,age,email} = {names:"Tisa", age: 20, email: "TiSa11@gmail.com"};
+console.log(names, age, email);
+// console.log(age);
+// console.log(email);
+
+//destructure fucntion parameters
+
+function gerrt ({name1, age}){
+    console.log(`Hello, ${name1} , your age is: ${age}`);
+}
+const user1 = {
+    name1: "TiSa",
+    age: 20,
+};
+gerrt(user1)
