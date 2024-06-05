fe224615aa53545c475fbb10888d48fe992bb622
utsav-baghela <utsavu858@gmail.com>
2024-06-05 20:24:29 +0530

    browser window objects

---
+++ Getting-started-js/DOM/getElementById/script.js
@@ -0,0 +1,13 @@
+//>>>>
+//Window Object
+//>>>>
+
+// console.log(window.document);
+// console.log(window.innerHeight);
+// console.log(window.innerWidth);
+// console.log(window.screen);
+// console.log(window.alert());
+// console.log(window.confirm());
+// console.log(window.history);
+// console.log(window.navigator);
+// console.log(window.location.host);
