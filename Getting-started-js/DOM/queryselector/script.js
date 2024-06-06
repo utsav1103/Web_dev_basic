d3754bc7437bdced10dfb12d94d92b54690896e3
utsav-baghela <utsavu858@gmail.com>
2024-06-06 17:41:06 +0530

    using queryselector method

---
+++ Getting-started-js/DOM/queryselector/script.js
@@ -0,0 +1,24 @@
+//get first product card
+//>>>>>>
+//query selector
+
+const firstProductCardEl = document.querySelector('.product-card');
+// console.log(firstProductCardEl);
+
+
+
+//highest rating product
+const highestRatingEl = document.querySelector("[data-rating='4.8']");
+// console.log(highestRatingEl);
+
+
+
+//laptop category product
+const laptopEl = document.querySelector("[data-category='laptop']");
+// console.log(laptopEl);
+
+
+//selecting an id
+const dashboardEl = document.querySelector("#dashboard");
+console.log(dashboardEl);
+
