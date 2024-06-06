1b15da7e1758dfcf63041872738e3c6d3d57a14f
utsav-baghela <utsavu858@gmail.com>
2024-06-06 15:18:59 +0530

    using getElementById

--- Getting-started-js/DOM/getElementById/script.js
+++ Getting-started-js/DOM/getElementById/script.js
@@ -11,3 +11,28 @@
 // console.log(window.history);
 // console.log(window.navigator);
 // console.log(window.location.host);
+
+
+
+
+
+//>>>>>selecting element using getElementById
+//>>>>>
+
+const conferenceTitleEl = document.getElementById("conferenceTitle");
+// console.log(conferenceTitle.innerHTML)
+
+const conferenceDateEl = document.getElementById("conferenceDate");
+
+const locationTitleEl = document.getElementById("locationTitle");
+
+const speakerListEl = document.getElementById("speakerList")
+
+// console.log({
+//     conferenceDateEl,
+//     conferenceTitleEl,
+//     locationTitleEl,
+//     speakerListEl,
+// });
+
+
