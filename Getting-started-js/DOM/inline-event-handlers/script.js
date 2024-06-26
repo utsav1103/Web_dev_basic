74981f68052b7f23f043ca5afac580098742d266
utsav-baghela <utsavu858@gmail.com>
2024-06-26 14:47:44 +0530

    using inline event handlers

---
+++ Getting-started-js/DOM/inline-event-handlers/script.js
@@ -0,0 +1,52 @@
+//____ mouse event ____//
+
+function clickHandler(){
+    // console.log("click event is called");
+}
+
+function dbClickHandler(){
+    // console.log("dbClickHandler event is called");
+}
+
+function onMouseOverHandler(){
+
+    // console.log("onMouseOverHandler event is called");
+}
+
+
+function onMouseOutHandler(){
+
+    // console.log("onMouseOutHandler event is called");
+}
+
+
+
+//>>>>> Keyboard event >>>>//
+
+
+function onkeydownHandler(){
+    
+    // console.log("onkeydownHandler event is called");
+}
+
+
+// function onkeyupHandler(){console.log("onkeyupHandler event is called");}
+
+
+function onkeypressHandler(){console.log("onkeypressHandler event is called");}
+
+
+
+//>>>>> Form event >>>>//
+
+function onsubmitHandler(){
+    console.log("onsubmitHandler event is called");
+}
+
+function onfocusHandler(){
+    console.log("onfocusHandler event is called");
+}
+
+function onblurHandler(){
+    console.log("onblurHandler event is called");
+}
