2e2946b90d3ab3d17fc807ee25b73bf04b38a437
utsav-baghela <utsavu858@gmail.com>
2024-07-29 10:14:45 +0530

    ES module code demo

---
+++ NODEJS/ESmodule/app.mjs
@@ -0,0 +1,20 @@
+// import greet from './utiils.mjs';
+
+// console.log(greet("Tissa"));
+
+
+
+
+// import {add, subtract} from "./utiils.mjs";
+
+// console.log(add(10, 5), subtract    (20, 15));
+
+
+
+import greet, {subtract, add} from './utiils.mjs';
+  
+  console.log(greet ("Tissa..."));
+  console.log(subtract(100,1));
+  console.log(add(60,39));
+  
+  
---
+++ NODEJS/ESmodule/utiils.mjs
@@ -0,0 +1,33 @@
+//?default export
+
+// export default function(name){
+//     return `Hella ${name}`;
+// }
+
+
+
+//?Name export
+
+// export function add(a,b){
+//     return a+b;
+// }
+
+// export function subtract(a,b){
+//     return a-b;
+// }
+
+
+//! MIxed  export(default + named)
+
+export default function(name){
+         return `Hella ${name}`;
+    }
+
+
+    export function add(a,b){
+             return a+b;
+         }
+        
+         export function subtract(a,b){
+             return a-b;
+         }
