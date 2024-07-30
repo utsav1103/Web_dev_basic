670c018686c9cc9f86e503418cbebc4688d14dd5
utsav-baghela <utsavu858@gmail.com>
2024-07-30 10:22:55 +0530

    path api

---
+++ NODEJS/Path-API/app.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+
+//* path.basename()
+//gives last path name
+// const filename = path.basename("/user/utsav/wev_dev/html/getting-started/tisa.txt");
+
+// console.log(filename);
+
+//!path.dirname()
+
+// const filename = path.dirname("/user/utsav/wev_dev/html/getting-started/tisa.txt");
+
+//  console.log(filename);
+
+//? path.extname()
+//gives extention name for example .txt, .js .pdf etc.
+// const filename = path.extname("/user/utsav/wev_dev/html/getting-started/tisa.txt");
+
+// console.log(filename);
+
+//* path.join()
+const jointPath = path.join('/user', 'test', 'file.txt'); 
