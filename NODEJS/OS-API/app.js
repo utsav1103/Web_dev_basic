16ffa0770526d24a42776c69aae24cfe870cfcff
utsav-baghela <utsavu858@gmail.com>
2024-07-30 10:07:25 +0530

    OS-api

---
+++ NODEJS/OS-API/app.js
@@ -0,0 +1,36 @@
+const os = require("os");
+
+//? os.arch()
+
+// console.log("CPU Architecture", os.arch());
+
+
+//! os.cpus()
+// console.log('cpu info', os.cpus());
+
+
+//* os.endianness()
+// console.log(os.endianness());
+
+
+//! os.freemem()
+// console.log("Free memory is",os.freemem() ,"bytes");
+
+//?os.homedir()
+// console.log(os.homedir());
+
+
+//*os.hostname()
+// console.log(os.hostname());
+
+//!os.loadavg()
+// console.log(os.loadavg());
+
+
+//? os.networkInterfaces
+// console.log(os.networkInterfaces());
+
+
+//* os.platform()
+console.log(os.platform());
+
