02b7d84686606a7049a640a393b21a1f7730282e
utsav-baghela <utsavu858@gmail.com>
2024-08-13 11:12:37 +0530

    application level middleware

--- Express/middlewares/server.js
+++ Express/middlewares/server.js
@@ -1,14 +1,50 @@
 const express = require("express");
 const app = express();
 const PORT = 5001;
+//!>>>>> Built in middlewares
+
+
 //? serve static files
 // app.use(express.static())
 
-//!pass incoming data
+//?pass incoming data
 app.use(express.json());
 
-//*passing incoming url encoded data
+//?passing incoming url encoded data
 app.use(express.urlencoded());
+
+//!Application level middlewares
+
+//*logging details of every request
+
+// app.use((req, res , next)=>{
+//     console.log(req.body);
+    
+//     console.log("An app level middleware");
+//     //call next
+//     next();
+    
+// });
+//another method to create middleware
+const logRequest = (req, res , next)=>{
+        console.log(`Request received at: ${new Date().toISOString()} for ${req.metho} to ${req.path}`);
+        
+        
+        //call next
+        next();
+        
+    };
+    app.use(logRequest);
+//Home route
+app.get("/", (req, res) =>{
+   console.log
+    (
+    req.body
+    );
+    res.json({
+        message: 'Welcome Dear tissa...',
+    })
+});
 
 //Create a book
 app.post("/books", (req, res) =>{
