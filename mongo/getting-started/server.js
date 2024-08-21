cc7c76de2ab0f85ff1828f51af758a95b9b461a2
utsav-baghela <utsavu858@gmail.com>
2024-08-21 09:41:35 +0530

    connecting to mongoDB

---
+++ mongo/getting-started/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const PORT = 5000;
+//connect to mongodb
+
+//username mongodb = utsavu858
+//password mongodb = eynD6Q6Q03hMtTKZ
+
+//mongodb url = mongodb+srv://utsavu858:eynD6Q6Q03hMtTKZ@utsav-cluster.osnhx.mongodb.net/students-database
+//?   1.create the client
+const client = new MongoClient("mongodb+srv://utsavu858:eynD6Q6Q03hMtTKZ@utsav-cluster.osnhx.mongodb.net/students-database", {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+//? 2. function to connect
+const connectDB = async() => {
+    try {
+        await client.connect();
+        console.log("MongoDB connected successfully");
+    }catch(error){
+        console.log(error);
+        
+    }
+};
+
+//? 3. Call the function
+connectDB();
+
+//start the server
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
