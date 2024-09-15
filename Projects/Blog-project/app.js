647424a999b674bb0bf98242227f69d38f30482c
utsav-baghela <utsavu858@gmail.com>
2024-09-15 14:18:23 +0530

    displaying login and register pages

--- Projects/Blog-project/app.js
+++ Projects/Blog-project/app.js
@@ -7,6 +7,17 @@ require ("dotenv").config();
 //port
 const PORT = process.env.PORT || 3000;
 
+//EJS
+app.set("view engine", "ejs");
+//routes
+app.get('/auth/login',(req, res) => {
+    res.render("login");
+});
+
+app.get('/auth/register',(req, res) => {
+    res.render("register");
+});
+
 mongoose.connect(process.env.MONGODB_URL).then(()=>{
     console.log("Database connected successfully");
     
