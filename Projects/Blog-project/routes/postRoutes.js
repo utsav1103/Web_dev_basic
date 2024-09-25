d1d6b4e8b48f4f93c021c484ad6355aea04f39dc
utsav-baghela <utsavu858@gmail.com>
2024-09-25 20:44:25 +0530

    isAuthenticated middleware

--- Projects/Blog-project/routes/postRoutes.js
+++ Projects/Blog-project/routes/postRoutes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const { getPostForm, createPost } = require("../controllers/postControllers");
 const { post } = require("./authRoutes");
 const upload = require("../config/multer");
+const { ensureAuthenticated } = require("../middlewares/auth");
 const postRoutes = express.Router();
 
 //get post form
 postRoutes.get("/add", getPostForm);
 
 //post logic
-postRoutes.post('/add',upload.array("images", 5), createPost);
+postRoutes.post('/add',ensureAuthenticated,upload.array("images", 5), createPost);
 
 module.exports = postRoutes;
